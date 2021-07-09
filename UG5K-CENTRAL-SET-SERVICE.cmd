246e48dddbffb0614670d93f2be501855bd41dda
Arturo <Arturo@b1d50d5f-df8f-9249-af92-6a0d583c2fd7>
2021-07-09 07:08:47 +0000

    Actualizacion del Modelo de Base de Datos y nuevo script de configuracion...
    git-svn-id: http://10.34.2.66/svn/svn_repository/REDAN-V/branches/agl-dev/ulises-gw-config-v3@9549 b1d50d5f-df8f-9249-af92-6a0d583c2fd7

---
+++ UG5K-CENTRAL-SET-SERVICE.cmd
@@ -0,0 +1,24 @@
+﻿:menu
+cls
+@echo off
+echo ****************************************************************
+echo **** Utilidad de Configuracion del servicio para Aplicacion ****
+echo **** Centralizada de Configuracion para REDAN V5            ****
+echo ****************************************************************
+echo.
+echo.
+choice /C SN /M "Crear el servicio Ulises G5000 ? (S)i (N)o" 
+if errorlevel 2 goto fin
+if errorlevel 1 goto instala-servicio
+goto menu
+:instala-servicio
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
+if %OS%==32BIT echo This is a 32bit operating system
+if %OS%==64BIT echo This is a 64bit operating system
+if %OS%==32BIT copy .\nss\win32\nssm.exe .
+if %OS%==64BIT copy .\nss\win64\nssm.exe .
+.\nssm.exe install Ulises-G5000
+
+:fin
+@echo Fin de la instalacion
+pause
