fb44d5065e882fce3097f8602970ecb21c6f942c
Arturo <Arturo@b1d50d5f-df8f-9249-af92-6a0d583c2fd7>
2021-07-30 08:40:09 +0000

    RM4881. Error en mensajes de error al borrar una configuración 'supervisada'. El error estaba mal mapeado en el servidor y además estaba en Ingles en la base de datos. Se ha corregido el error del servidor y se ha reprogramado el Trigger de la base de datos.
    git-svn-id: http://10.34.2.66/svn/svn_repository/REDAN-V/branches/agl-dev/ulises-gw-config-v3@9615 b1d50d5f-df8f-9249-af92-6a0d583c2fd7

---
+++ data_model/RM4881-ug5kv2-configurations-triggers.sql
@@ -0,0 +1,29 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         10.5.8-MariaDB - mariadb.org binary distribution
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             11.0.0.5919
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Volcando estructura para disparador ug5kv2.configuraciones_BEFORE_DELETE
+SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
+DELIMITER //
+CREATE TRIGGER `configuraciones_BEFORE_DELETE` BEFORE DELETE ON `configuraciones` FOR EACH ROW BEGIN
+DECLARE specialty CONDITION FOR SQLSTATE '45001';
+	IF old.activa=1 THEN
+		SIGNAL SQLSTATE '45000'
+		SET MESSAGE_TEXT = 'No se pueden borrar las configuraciones supervisadas', MYSQL_ERRNO = 1001;
+      END IF;
+END//
+DELIMITER ;
+SET SQL_MODE=@OLDTMP_SQL_MODE;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
