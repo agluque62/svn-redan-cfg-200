27a11b4d3eb99179d62333018b89ce9b981dcae4
Arturo Garcia <arturo.garcia.luque@gmail.com>
2023-02-13 12:29:01 +0100

    ExternalResources refactorizado
    Signed-off-by: Arturo Garcia <arturo.garcia.luque@gmail.com>

--- lib2/Testing/index.js
+++ lib2/Testing/index.js
@@ -1,8 +1,18 @@
 
+var dbutils = require('../srv/lib/dbasync');
 // require("./Tests/loggerTests").ExecuteTest();
 // require('./Tests/dbAsyncTest').ExecuteTest();
 // require('./Tests/authenticationTests').Test(); TODO
 // require('./Tests/httpclientTests').test();
 // require('./Tests/sipr2mngTest').test();
 // require('./Tests/versionTest').test();
-require('./Tests/Get_Cfg_ReportTests').test();
+// require('./Tests/Get_Cfg_ReportTests').test();
+require('./Tests/ExternalEqTest').test();
+
+var dbAccessTemplate = async () => {
+    var res = {};
+    var db = dbutils.makeDbAsync();
+    await dbutils.dbTransaction(db, async () =>{
+    }); 
+    return res;
+}
